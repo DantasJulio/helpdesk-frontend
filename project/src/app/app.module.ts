4d5d8977df0f6dc519fccfb45faebc0a56e84c81
Julio Dantas <julio.dantas@mv.com.br>
2023-07-17 15:32:23 -0300

    PROJETO-00001 | Criando o component NAV e adicionando a rota.

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -21,10 +21,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
